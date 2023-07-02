5d188717d936a25108c3a58e42e3bbaf2852befa
Rebecca Biju <biju.r@northeastern.edu>
2023-07-02 14:34:41 -0400

    Skills docs update links

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -9,60 +9,60 @@ export const skills = [
         id: 1,
         title: 'NodeJS',
         img: '/assets/node.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://nodejs.org/en/docs'
     },
     {
         id: 2,
         title: 'MongoDB',
         img: '/assets/mongo.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://www.mongodb.com/docs/'
     },
     {
         id: 3,
         title: 'ReactJS',
         img: '/assets/react.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://legacy.reactjs.org/'
     },
     {
         id: 4,
         title: 'Java',
-        img: '../assets/java.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        img: '/assets/java.png',
+        url: 'https://docs.oracle.com/en/java/'
     },
     {
         id: 5,
         title: 'AWS',
         img: '/assets/aws.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://docs.aws.amazon.com/'
     },
     {
         id: 6,
         title: 'Terraform',
         img: '/assets/terraform.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://developer.hashicorp.com/terraform/docs'
     },
     {
         id: 7,
         title: 'GitHub',
         img: '/assets/github.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://docs.github.com/en'
     },
     {
         id: 8,
         title: 'Figma',
         img: '/assets/figma.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://help.figma.com/hc/en-us'
     },
     {
         id: 9,
         title: 'HTML',
         img: '/assets/html.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
     },
     {
         id: 10,
         title: 'CSS',
         img: '/assets/css.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        url: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
     }
 ]
