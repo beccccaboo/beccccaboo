175a5167e94773868c2fd3bf0f527819d94e4f10
Rebecca Biju <biju.r@northeastern.edu>
2024-09-12 15:04:22 -0400

    feat(project): Update project

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,6 +1,16 @@
 export const projects = [
     {
-      id: 1,
+        id: 1,
+        name: 'Kubernetes DevOps Automation Suite',
+        date: '2023',
+        tech: ['Kubernetes', 'Jenkins', 'Kafka', 'Terraform', 'CI/CD', 'Packer', 'Python', 'PostgreSQL', 'Kuernetes Operator', 'Golang'],
+        tagline:
+            'This project presents an automated solution for deploying cloud-native applications, utilizing Infrastructure as Code (IaC), Kubernetes, Helm charts, CI/CD pipelines, and modern DevOps methodologies.',
+        source: 'https://github.com/beccccaboo/Kubernetes-DevOps-Automation-Suite',
+
+    },
+    {
+      id: 2,
       name: 'WebApp (CloudOps)',
       date: '2023',
       tech: ['EC2', 'VPC', 'CloudWatch', 'IAM', 'RDS', 'S3', 'Terraform', 'Packer', 'NodeJS', 'PostgreSQL', 'ExpressJS' ],
@@ -9,7 +19,7 @@ export const projects = [
       source: 'https://github.com/orgs/CloudComputingSpringCSYE6225/repositories',
     },
     {
-      id: 2,
+      id: 3,
       name: 'Editor Hive',
       date: '2023',
       tech: ['NodeJS', 'MongoDB', 'ReactJS', 'ExpressJS', 'Redux'],
@@ -17,7 +27,7 @@ export const projects = [
       source: 'https://github.com/beccccaboo/editor-hive',
     },
     {
-      id: 3,
+      id: 4,
       name: 'Amazon Community',
       date: '2023',
       tech: ['Figma'],
@@ -26,7 +36,7 @@ export const projects = [
       demo: 'https://www.figma.com/proto/NXg3MApoWm5exRtTEmzNiz/RebeccaBiju_Spring2023?page-id=1361%3A1100&type=design&node-id=1361-1102&viewport=219%2C419%2C0.15&scaling=scale-down&starting-point-node-id=1361%3A1102&mode=design'
     },
     {
-      id: 4,
+      id: 5,
       name: "Bizconnect",
       date: '2023',
       tech: ['Figma'],
@@ -35,7 +45,7 @@ export const projects = [
       demo: 'https://www.figma.com/proto/NXg3MApoWm5exRtTEmzNiz/RebeccaBiju_Spring2023?page-id=1210%3A2792&type=design&node-id=1210-2824&viewport=456%2C350%2C0.14&scaling=min-zoom&starting-point-node-id=1210%3A2831&mode=design'
     },
     {
-      id: 5,
+      id: 6,
       name: 'Stubber',
       date: '2021',
       tech: ['NodeJS', 'MongoDB', 'ReactJS', 'ExpressJS', 'Docker'],
@@ -43,7 +53,7 @@ export const projects = [
       source: 'https://github.com/beccccaboo/Stubber'
     },
     {
-      id: 6,
+      id: 7,
       name: 'Birthday Mobile Application',
       date: '2020',
       tech: ['Flutter', 'Android Studio', 'JavaScript'],
@@ -51,7 +61,7 @@ export const projects = [
       source: 'https://github.com/beccccaboo/Birthday-Wish-App',
     },
     {
-      id: 7,
+      id: 8,
       name: 'Undergrad Yearbook',
       date: '2020',
       tech: ['Adobe InDesign'],
@@ -59,7 +69,7 @@ export const projects = [
       demo: 'https://drive.google.com/file/d/10HKUAsJMYazz3Mp_ZdvCHDAluu7gOyzK/view?usp=sharing',
     },
     {
-        id: 8,
+        id: 9,
         name: 'Multimodal Image Fusion',
         date: '2020',
         tech: ['Python'],
